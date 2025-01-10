440f00e3359e1034026b8862ef23e4dca1c2eaf0
Niklas Brodén <nickebrodde@gmail.com>
2025-01-10 10:53:29 +0100

    Initial commit: Add Microchip Studio project files and main.c
    - Added main.c as the starting point for the project.
    - Included Microchip Studio project configuration files:
      - .atsuo: User options for the project.
      - .atsln: Solution file for the project.
      - .cproj: Project file containing settings and paths.
      - componentinfo.xml: Metadata for components.
    - Sets up the foundation for further development.

---
+++ TempHumMonitor/TempHumMonitor/main.c
@@ -0,0 +1,18 @@
+/*
+ * TempHumMonitor.c
+ *
+ * Created: 10/01/2025 10:53:09
+ * Author : brode
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
